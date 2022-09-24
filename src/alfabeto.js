f90a9d1608f18206d1710fc2c389eccc7fe5e24a
Gustavo <gustavolerner.hartmann@gmail.com>
2022-09-24 03:53:16 -0300

    Soretear palavra, clicar na tecla e fim de jogo

---
+++ src/alfabeto.js
@@ -0,0 +1,107 @@
+const alfabeto = [
+    {
+        letra: "a",
+        booleano: true
+    },
+    {
+        letra: "b",
+        booleano: true
+    },
+    {
+        letra: "c",
+        booleano: true
+    },
+    {
+        letra: "d",
+        booleano: true
+    },
+    {
+        letra: "e",
+        booleano: true
+    },
+    {
+        letra: "f",
+        booleano: true
+    },
+    {
+        letra: "g",
+        booleano: true
+    },
+    {
+        letra: "h",
+        booleano: true
+    },
+    {
+        letra: "i",
+        booleano: true
+    },
+    {
+        letra: "j",
+        booleano: true
+    },
+    {
+        letra: "k",
+        booleano: true
+    },
+    {
+        letra: "l",
+        booleano: true
+    },
+    {
+        letra: "m",
+        booleano: true
+    },
+    {
+        letra: "n",
+        booleano: true
+    },
+    {
+        letra: "o",
+        booleano: true
+    },
+    {
+        letra: "p",
+        booleano: true
+    },
+    {
+        letra: "q",
+        booleano: true
+    },
+    {
+        letra: "r",
+        booleano: true
+    },
+    {
+        letra: "s",
+        booleano: true
+    },
+    {
+        letra: "t",
+        booleano: true
+    },
+    {
+        letra: "u",
+        booleano: true
+    },
+    {
+        letra: "v",
+        booleano: true
+    },
+    {
+        letra: "w",
+        booleano: true
+    },
+    {
+        letra: "x",
+        booleano: true
+    },
+    {
+        letra: "y",
+        booleano: true
+    },
+    {
+        letra: "z",
+        booleano: true
+    }];
+
+export default alfabeto
--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,93 @@
+import React from "react"
+import alfabeto from "../alfabeto";
+import palavras from "../palavras"
+
 export default function App() {
+    const [jogoIniciado, setJogoIniciado] = React.useState(false);
+    const [numeroErros, setNumeroErros] = React.useState(0);
+    const [palavraSorteada, setPalavraSorteada] = React.useState("");
+    const [arrayPalavra, setArrayPalavra] = React.useState([]);
+    const [arrayAlfabeto, setArrayAlfabeto] = React.useState(alfabeto);
+
+    function Tecla(props) {
+        return (
+            <button className="tecla" disabled={props.booleano} onClick={clicarTecla}>{props.letra.toUpperCase()}</button>
+        )
+
+    }
+
+    function comecarJogo() {
+        document.querySelector(".palavra-escolhida").classList.remove("errado")
+        document.querySelector(".palavra-escolhida").classList.remove("certo")
+        setNumeroErros(0)
+        setJogoIniciado(true);
+        alfabeto.map((obj) => obj.booleano = false)
+        sortearPalavra();
+    }
+
+    function sortearPalavra() {
+        const numeroAleatorio = Math.floor(Math.random() * palavras.length)
+        setPalavraSorteada(palavras[numeroAleatorio])
+        console.log(palavras[numeroAleatorio]);
+        setArrayPalavra(Array(palavras[numeroAleatorio].length).fill("_"))
+    }
+
+    function clicarTecla(evento) {
+        const tecla = evento.target.innerText.toLowerCase();
+        palavraSorteada.includes(tecla) ? reenderizarLetra(tecla) : aumentarErros()
+        setArrayAlfabeto(alfabeto.map((obj) => obj.letra === evento.target.innerText.toLowerCase() || obj.booleano === true ? obj.booleano = true : obj.booleano = false))
+    }
+
+    function reenderizarLetra(tecla) {
+        const arrayPalavraSorteada = palavraSorteada.split("")
+        arrayPalavraSorteada.map((l, indexLetra) => l === tecla ? arrayPalavra.splice(indexLetra, 1, l) : console.log("batata"))
+        if (!arrayPalavra.includes("_")) {
+            ganharJogo();
+        }
+    }
+
+    function ganharJogo() {
+        resetarEstados();
+        document.querySelector(".palavra-escolhida").classList.add("certo")
+    }
+
+    function aumentarErros() {
+        const qtdErros = numeroErros + 1
+        setNumeroErros(qtdErros)
+        if (qtdErros === 6) {
+            finalizarJogo();
+        }
+    }
+
+    function finalizarJogo() {
+        resetarEstados()
+        document.querySelector(".palavra-escolhida").classList.add("errado")
+    }
+
+    function resetarEstados() {
+        setArrayPalavra(palavraSorteada)
+        alfabeto.map((obj) => obj.booleano = true)
+        setJogoIniciado(false);
+    }
+
     return (
         <div className="conteudo">
             <div className="conteudo-jogo">
-                <img src="./assets/img/forca0.png" alt="imagem da forca" />
+                <img src={`./assets/img/forca${numeroErros}.png`} alt="imagem da forca" />
                 <div className="info-jogo">
-                    <button className="botao-escolher-palvra">Escolher palavra</button>
+                    <button className="botao-escolher-palvra" onClick={comecarJogo}>Escolher palavra</button>
                     <div className="palavra-escolhida">
-                        <h1>________</h1>
+                        <h1>{arrayPalavra}</h1>
                     </div>
                 </div>
             </div>
             <div className="interface-jogo">
                 <div className="teclado">
-                    <div className="tecla">A</div>
-                    <div className="tecla">B</div>
-                    <div className="tecla">C</div>
-                    <div className="tecla">D</div>
-                    <div className="tecla">E</div>
-                    <div className="tecla">F</div>
-                    <div className="tecla">G</div>
-                    <div className="tecla">H</div>
-                    <div className="tecla">I</div>
-                    <div className="tecla">J</div>
-                    <div className="tecla">K</div>
-                    <div className="tecla">L</div>
-                    <div className="tecla">M</div>
-                    <div className="tecla">N</div>
-                    <div className="tecla">O</div>
-                    <div className="tecla">P</div>
-                    <div className="tecla">Q</div>
-                    <div className="tecla">R</div>
-                    <div className="tecla">S</div>
-                    <div className="tecla">T</div>
-                    <div className="tecla">U</div>
-                    <div className="tecla">V</div>
-                    <div className="tecla">W</div>
-                    <div className="tecla">X</div>
-                    <div className="tecla">Y</div>
-                    <div className="tecla">Z</div>
+                    {alfabeto.map((v, indexValor) => <Tecla letra={v.letra} key={indexValor} booleano={v.booleano} />)}
                 </div>
                 <div className="chute">
                     <h2>Já sei a palavra!</h2>
-                    <input type="text" />
+                    <input type="text" disabled={!jogoIniciado} />
                     <button className="botao-chutar">Chutar</button>
                 </div>
             </div>
